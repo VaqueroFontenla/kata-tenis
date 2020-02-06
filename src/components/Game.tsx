3de4c8680297f4321e819c45fd7e90be254bdba8
VaqueroFontenla <loret.vaquero@gmail.com>
2020-02-06 13:05:02 +0100

    Remove addPoint funciotn

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -12,18 +12,54 @@ interface Props {
   
 export const Game = (props: Props) => {
     const className = '';
+    const [isWinner, setIsWinner] = React.useState(false)
+    const [ isDeuce, setIsDeuce] = React.useState(false)
     const [ scorePlayerOne, setScorePlayerOne] = React.useState(0);
     const [ scorePlayerTwo, setScorePlayerTwo] = React.useState(0);
-    const wonPoint = (playerName : string) => {
-     console.log('hola')
+
+    React.useEffect(()=> {
+            if (scorePlayerOne === 40 && scorePlayerTwo === 40) {
+                setIsDeuce(true);
+            
+        }
+    },[scorePlayerOne,scorePlayerTwo]); 
+
+    const getScore = (scorePlayer: number): any  => {
+        switch(scorePlayer) {
+            case 0: {
+                return 15;
+            }
+            case 15: {
+                return 30
+            }
+            case 30: {
+                return 40
+            }
+            case 40: {
+                return setIsWinner(true);               
+            }
+                
+        }
     }
-    const addPointPlayerTwo = () => {}
+    const wonPoint = (playerName : string) => {
+        if (playerName === props.namePlayerOne)
+            setScorePlayerOne(getScore(scorePlayerOne));
+        else
+            setScorePlayerTwo(getScore(scorePlayerTwo));
+    }
+
+
+    const resetGame = () => {
+        setScorePlayerOne(0);
+        setScorePlayerTwo(0);
+    }
+  
     return (
         <div>
             <div>
                 {props.namePlayerOne}
                 <Button 
-                    onClick={wonPoint}
+                    onClick={()=> wonPoint(props.namePlayerOne)}
                     className={className}
                 >
                     Won Point
@@ -39,6 +75,19 @@ export const Game = (props: Props) => {
                 </Button>
                 Score {props.namePlayerTwo}: {scorePlayerTwo}
             </div>
+           { isWinner && 
+           <div>
+               Winner
+               <Button 
+                    onClick={()=> resetGame()}
+                    className={className}> 
+                    Reset Game
+                </Button>
+            </div>}
+            { isDeuce && 
+           <div>
+             Iguales mangurrino
+            </div>}
         </div>
     )    
 }
