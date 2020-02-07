e09bbc795c753f2582fbb7fc798ace9c9c0a7604
VaqueroFontenla <loret.vaquero@gmail.com>
2020-02-07 11:38:39 +0100

    Array score

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -7,18 +7,30 @@ interface Props {
     namePlayerTwo: string
 }
 
+interface State {
+    score: string;
+    scoreList: string[];
+    scorePlayerOne: number;
+    scoscorePlayerTwo: number;
+    showPoints: boolean;
+    pointsPlayerOne: number;
+    pointsPlayerTwo: number;
+}
+
 
 //Hacer interface de State
   
 export const Game = (props: Props) => {
 
-    const [ score, setScore] = React.useState('')
-    const [ scorePlayerOne, setScorePlayerOne] = React.useState(0);
-    const [ scorePlayerTwo, setScorePlayerTwo] = React.useState(0);
-    const [ showPoints, setShowPoints] = React.useState(true);
-    const [ pointsPlayerOne, setPointsPlayerOne] = React.useState(0);
-    const [ pointsPlayerTwo, setPointsPlayerTwo] = React.useState(0);
-    console.log(score);
+    const [ score, setScore] = React.useState<string>('');
+    const [ scoreList, setScoreList] = React.useState<string[]>([''])
+    const [ scorePlayerOne, setScorePlayerOne] = React.useState<number>(0);
+    const [ scorePlayerTwo, setScorePlayerTwo] = React.useState<number>(0);
+    const [ showPoints, setShowPoints] = React.useState<boolean>(true);
+    const [ pointsPlayerOne, setPointsPlayerOne] = React.useState<number>(0);
+    const [ pointsPlayerTwo, setPointsPlayerTwo] = React.useState<number>(0);
+
+    
 
     const transformScore = (score:number) => {
         switch(score) {
@@ -44,7 +56,8 @@ export const Game = (props: Props) => {
             if (differenceScore === 0 ) {
                 switch(scorePlayerOne && scorePlayerTwo) {
                     case 0: 
-                        setScore('Start game');
+                        setScore('Love all');
+                        break;
                     case 1: 
                         setScore('Fifteen all');
                         break;
@@ -79,8 +92,15 @@ export const Game = (props: Props) => {
             } else {   
                 setScore(`${transformScore(scorePlayerOne)}-${transformScore(scorePlayerTwo)}`)
             }
+
         
     },[scorePlayerOne,scorePlayerTwo]); 
+
+    React.useEffect(()=> {
+        let newScore: string[] = [...scoreList, score];
+        setScoreList(newScore);
+
+    },[score])
 
     const checkPoinstPlayer = (scorePlayer: number): any => {
         switch(scorePlayer) {
@@ -113,39 +133,42 @@ export const Game = (props: Props) => {
     const resetGame = () => {
         setScorePlayerOne(0);
         setScorePlayerTwo(0);
+        setScoreList([]);
     }
   
     return (
-        <div className="game">
-            
-            <div className="game-player">
-                <span className="game-player__label">{props.namePlayerOne}</span>
-                <Button 
-                    onClick={()=> wonPoint(props.namePlayerOne)}
-                    className="button button__start"
-                >
-                    Won Point
-                </Button>
-                <span className="game-player__score">{pointsPlayerOne}</span>
-            </div>
+        <div className="game-container">
+            <div className="game">
+                <div className="game-player">
+                    <span className="game-player__label">{props.namePlayerOne}</span>
+                    <Button 
+                        onClick={()=> wonPoint(props.namePlayerOne)}
+                        className="button button__start"
+                    >
+                        Won Point
+                    </Button>
+                    <span className="game-player__score">{pointsPlayerOne}</span>
+                </div>
 
-            <div className="game-score">
-                <span className="score">{score}</span>
-            </div>
+                <div className="game-score">
+                    {
+                     scoreList.map( score =><span className="score">{score}</span> )
+                    }
+                </div>
 
-            <div className="game-player">
-                <span  className="game-player__label">{props.namePlayerTwo}</span>
-                <Button 
-                    onClick={()=> wonPoint(props.namePlayerTwo)}
-                    className="button button__start"
-                > 
-                    Won  Point
-                </Button>
-                <span className="game-player__score">{pointsPlayerTwo}</span>
-  
+                <div className="game-player">
+                    <span  className="game-player__label">{props.namePlayerTwo}</span>
+                    <Button 
+                        onClick={()=> wonPoint(props.namePlayerTwo)}
+                        className="button button__start"
+                    > 
+                        Won  Point
+                    </Button>
+                    <span className="game-player__score">{pointsPlayerTwo}</span>
+    
+                </div>
             </div>
-           
-            <div className="score__container">
+            <div className="reset">
                <Button 
                     onClick={()=> resetGame()}
                     className= "button button__reset"       
