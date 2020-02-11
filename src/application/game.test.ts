afaed3595675974d986c4bcde75e647d92ca3a53
VaqueroFontenla <loret.vaquero@gmail.com>
2020-02-11 10:35:41 +0100

    Prettier

--- src/application/game.test.ts
+++ src/application/game.test.ts
@@ -1,27 +1,27 @@
 import { wonPoint, constructor, getFormattedScore } from "./game";
 
 describe("Game functionality", () => {
-    beforeEach(() => {
-      constructor("One", "Two");
-    });
-    it("player one win first point", () => {
-      wonPoint("One");
-      const result = getFormattedScore(1,0);
-      expect(result).toBe("Fifteen - Love");
-    });
-    it("player two win first point", () => {
-      wonPoint("Two");
-      const result = getFormattedScore(0,1);
-      expect(result).toBe("Love - Fifteen");
-    });
-    it("player one win match", () => {
-      wonPoint("One");
-      const result = getFormattedScore(5,3);
-      expect(result).toBe("Winner One");
-    });
-    it("player two win match", () => {
-      wonPoint("Two");
-      const result = getFormattedScore(3,5);
-      expect(result).toBe("Winner Two");
-    });
+  beforeEach(() => {
+    constructor("One", "Two");
+  });
+  it("player one win first point", () => {
+    wonPoint("One");
+    const result = getFormattedScore(1, 0);
+    expect(result).toBe("Fifteen - Love");
+  });
+  it("player two win first point", () => {
+    wonPoint("Two");
+    const result = getFormattedScore(0, 1);
+    expect(result).toBe("Love - Fifteen");
+  });
+  it("player one win match", () => {
+    wonPoint("One");
+    const result = getFormattedScore(5, 3);
+    expect(result).toBe("Winner One");
+  });
+  it("player two win match", () => {
+    wonPoint("Two");
+    const result = getFormattedScore(3, 5);
+    expect(result).toBe("Winner Two");
+  });
 });
--- src/application/game.ts
+++ src/application/game.ts
@@ -2,68 +2,68 @@ const scoreArray: string[] = ["Love", "Fifteen", "Thirty", "Forty"];
 
 let scorePlayerOne: number = 0;
 let scorePlayerTwo: number = 0;
-let playerOneName: string = '';
-let playerTwoName: string ='';
+let playerOneName: string = "";
+let playerTwoName: string = "";
 
 export const constructor = (playerOne: string, playerTwo: string) => {
-	playerOneName = playerOne;
-	playerTwoName = playerTwo;
+  playerOneName = playerOne;
+  playerTwoName = playerTwo;
 };
 
 export const wonPoint = (playerName: string): void => {
-	if (playerName === playerOneName) {
-		scorePlayerOne++;
-	}
-	if (playerName === playerTwoName) {
-		scorePlayerTwo++;
-	}
+  if (playerName === playerOneName) {
+    scorePlayerOne++;
+  }
+  if (playerName === playerTwoName) {
+    scorePlayerTwo++;
+  }
 };
 
-export const transformScore = (score: number) => scoreArray[score]; 
+export const transformScore = (score: number) => scoreArray[score];
 
 export const selectWinner = (scoreDifference: number) => {
-	let winner: string = '';
-			if (scoreDifference > 0 ) {
-					winner = playerOneName
-			}
-			if (scoreDifference < 0 ) {
-					winner = playerTwoName
-			}
-			return winner
-}
+  let winner: string = "";
+  if (scoreDifference > 0) {
+    winner = playerOneName;
+  }
+  if (scoreDifference < 0) {
+    winner = playerTwoName;
+  }
+  return winner;
+};
 
-export const getFormattedScore = (scoreOne: number, scoreTwo: number): string => {
+export const getFormattedScore = (
+  scoreOne: number,
+  scoreTwo: number
+): string => {
+  let scoreDifference: number = scoreOne - scoreTwo;
+  let scoreString: string = "";
 
-	let scoreDifference: number = scoreOne - scoreTwo;
-	let scoreString: string = '';
+  if (scoreDifference === 0) {
+    if (scoreOne < 3) {
+      scoreString = `${scoreArray[scoreOne]} - all`;
+    }
+  } else {
+    if (scoreOne >= 3 || scoreTwo >= 3) {
+      const winner = selectWinner(scoreDifference);
+      if (scoreDifference === 0) {
+        scoreString = scoreString = "Deuce";
+      }
 
-			if( scoreDifference === 0 ) {
-					if(scoreOne < 3) {
-							scoreString =  `${scoreArray[scoreOne]} - all`
-					}
-			} else {
-					if (scoreOne >= 3 || scoreTwo >= 3) {
-							const winner = selectWinner(scoreDifference);
-							if (scoreDifference === 0 ) {
-									scoreString = scoreString = 'Deuce'
-							}
+      if (scoreDifference === 1 || scoreDifference === -1) {
+        scoreString = ` Advantage ${winner}`;
+      }
 
-							if(scoreDifference === 1 || scoreDifference === -1 ) {
-									scoreString = ` Advantage ${winner}`
-							}
-
-							if(scoreDifference === 2 || scoreDifference === -2 ) {
-									scoreString = `Winner ${winner}`
-							}
-					} else {
-							scoreString = `${transformScore(scoreOne)} - ${transformScore(scoreTwo)}`
-
-					}
-
-			}
-	return scoreString
-}
+      if (scoreDifference === 2 || scoreDifference === -2) {
+        scoreString = `Winner ${winner}`;
+      }
+    } else {
+      scoreString = `${transformScore(scoreOne)} - ${transformScore(scoreTwo)}`;
+    }
+  }
+  return scoreString;
+};
 
 export const getScore = (): string => {
-	return getFormattedScore(scorePlayerOne,scorePlayerTwo)
+  return getFormattedScore(scorePlayerOne, scorePlayerTwo);
 };
